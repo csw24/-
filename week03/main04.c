9521391ccd1b2dcb7fdfb95909ea4838b67ebca8
csw24 <csw24@sookmyung.ac.kr>
2024-09-25 14:14:44 +0900

    04

---
+++ week03/main04.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void) {
+	float a, b, div;
+	
+	printf("enter two integers: ");
+	scanf("%f" "%f", &a, &b);
+	
+	div = a/b;
+	
+	printf("%f / %f = %f", a, b, div);
+	
+	return 0;
+}
