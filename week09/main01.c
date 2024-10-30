121e124308e7532123fcd7206b870799726fb16a
csw24 <csw24@sookmyung.ac.kr>
2024-10-30 14:06:54 +0900

    week09

---
+++ week09/main01.c
@@ -0,0 +1,29 @@
+/*
+#include <stdio.h>
+
+int main(void) {
+	int flag = 1;
+	while(flag != 0)
+	{
+		int y;
+		y = 3;
+		flag = 0;
+	}
+	y = 4;
+	return 0;
+}
+*/
+
+#include <stdio.h>
+
+int main(void) {
+	int flag = 1;
+	int y;
+	while(flag != 0)
+	{
+		y = 3;
+		flag = 0;
+	}
+	y = 4;
+	return 0;
+}
